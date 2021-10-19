99dcb3a7b1bffa16d5fba192a31fa1fbd7e83525
Srimanth-design <83207593+Srimanth-design@users.noreply.github.com>
2021-10-19 01:52:51 -0700

    Update Greatest.java

--- BasicAssignments/src/com/shristi/basic/Greatest.java
+++ BasicAssignments/src/com/shristi/basic/Greatest.java
@@ -7,17 +7,7 @@ public class Greatest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		// Greatest number using the terinery operator
-
-		/*
-		 * int first_number = 100, second_number = 145, third_number = 172; String
-		 * great_number = (first_number > second_number && first_number > third_number)
-		 * ? "First number is the greatest" : (second_number > third_number) ?
-		 * "Second number is the greatest" : "Third number is the greatest";
-		 * 
-		 * System.out.println(great_number);
-		 */
-
+		//greatest number
 		Scanner input = new Scanner(System.in);
 		System.out.println("No of elements: ");
 		int number = input.nextInt();
