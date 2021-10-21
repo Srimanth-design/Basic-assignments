f0587822d781babed5431660cce772bea48fe6d2
Srimanth-design <83207593+Srimanth-design@users.noreply.github.com>
2021-10-21 23:36:58 +0530

    Committed day 3 work

---
+++ Account.java
@@ -0,0 +1,29 @@
+package com.training.AssignmentsDay2;
+
+public class Account {
+
+	double balance;
+
+	public Account(double balance) {
+		super();
+		this.balance = balance;
+	}
+
+	void withdraw(int amount) {
+		balance = balance - amount;
+	}
+
+	void deposit(int amount) {
+		balance = balance + amount;
+	}
+
+	double getBalance() {
+		return balance;
+	}
+
+	public void balanceCheck() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
